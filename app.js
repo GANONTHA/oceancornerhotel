a8cde8d8c9bba7d8eb7923637a97977c272ba3d1
Bruno <Bruno>
2023-12-05 12:35:57 +0100

    finalizing the small screen version of the website

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const spans = document.querySelectorAll(".burger-menu");
 const lists = document.querySelectorAll(".nav-menu");
 const navList = document.querySelector("ul");
 const burgerMenu = document.querySelector(".burger-menu");
+const navLinks = document.querySelectorAll(".mobile-menu ul li");
 
 burgerMenu.addEventListener("click", () => {
   //Toggling the class Open in each element in order to make the menu disappear onlick and appear again after clicking again
@@ -12,4 +13,22 @@ burgerMenu.addEventListener("click", () => {
     menu.classList.toggle("open");
   });
   navList.classList.toggle("open");
+
+  navLinks.forEach((li) => {
+    li.classList.remove("close");
+  });
+});
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.forEach((li) => {
+      li.classList.add("close");
+    });
+    spans.forEach((span) => {
+      span.classList.toggle("open");
+    });
+    lists.forEach((menu) => {
+      menu.classList.toggle("open");
+    });
+    navList.classList.toggle("open");
+  });
 });
